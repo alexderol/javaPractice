06b660c67a140c98e19f6bbbe7215d5bcd922817
alexderol <erolldemir45@gmail.com>
2022-10-03 18:53:15 +0300

    Practice

--- src/day04/Q12_StringManipulation.java
+++ src/day04/Q12_StringManipulation.java
@@ -9,11 +9,18 @@ public class Q12_StringManipulation {
         Scanner scan = new Scanner(System.in);
         System.out.println("bir kelime girin");
         String word = scan.nextLine();
-        if (word.contains(" ")){
-            System.out.println("girdi boşluk  içeriyor");
-        }if (word.length()>=0){
-            System.out.println("girdi  dutluk değil :)");
-        }else if (word.length()==0) System.out.println("girdi dutluk :)");
+       //if (word.contains(" ")){
+       //    System.out.println("girdi boşluk  içeriyor");
+       //}if (word.length()>=0){
+       //    System.out.println("girdi  dutluk değil :)");
+       //}else if (word.length()==0) System.out.println("girdi dutluk :)");
+        int bosluk = word.indexOf(" ");//boşluğun olup olmadığını -1 ise boşluk yoktur demek yani olmayan bir karakterin yokluğunda -1 döner
+
+        if (bosluk==-1 && !word.isEmpty())//boşluk karakteri yoksa ve strign ifadem bos değilse demek çok önemli burası özellikle word ün önüne konan ! ilk kez gördüm
+        {
+            System.out.println("sartlara uygun");
+
+        }else System.out.println("şartlara uygun değildir");
 
 
 
