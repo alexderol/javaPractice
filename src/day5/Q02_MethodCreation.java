70e2b4442b72219050556ebd2ca50fcc6345df60
alexderol <erolldemir45@gmail.com>
2022-10-04 18:20:20 +0300

    04/10/22

--- src/day5/Q02_MethodCreation.java
+++ src/day5/Q02_MethodCreation.java
@@ -1,16 +1,24 @@
 package day5;
 
+import java.util.Scanner;
+
 public class Q02_MethodCreation {
     /* Write a method named isEven that accepts an integer argument.
  The method should return true if the argument is even, or false otherwise.*/
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        int sayi= scan.nextInt();
+
+        System.out.println(isEven(sayi));
 
 
+    }
+
+    public static boolean isEven(int sayi) {
 
 
-
-
-
+        return sayi%2==0 ? true: false ;
     }
 
 
---
+++ src/day5/Q03_MethodCreation.java
@@ -0,0 +1,4 @@
+package day5;
+
+public class Q03_MethodCreation {
+}
