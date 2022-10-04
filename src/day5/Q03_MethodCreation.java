9637677dce3fbf1d967c728da9392b749162a708
alexderol <erolldemir45@gmail.com>
2022-10-04 18:31:12 +0300

    new

--- src/day5/Q03_MethodCreation.java
+++ src/day5/Q03_MethodCreation.java
@@ -1,4 +1,29 @@
 package day5;
 
+import java.util.Scanner;
+
 public class Q03_MethodCreation {
+/*
+ cm olarak verilen bir sayiyi, metre ve kilometreye donusturen bir method yaziniz
+  */
+public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    System.out.print("bir sayı giriniz (cm): ");
+    double sayi= scan.nextInt();
+    System.out.println(metrecevirici(sayi));
+    System.out.println(kmcevirici(sayi));
+
+
+}
+
+    private static double metrecevirici(double sayi) {
+
+        return sayi/100;
+    }
+
+    private static double kmcevirici(double sayi) {
+
+        return sayi/100000;
+    }
+
 }
