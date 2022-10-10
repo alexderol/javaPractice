f443356cc1fdbd7253cb039a86446356127dac7e
alexderol <erolldemir45@gmail.com>
2022-10-10 19:21:12 +0300

    go

--- src/day06/Q05_ForLoop.java
+++ src/day06/Q05_ForLoop.java
@@ -1,4 +1,32 @@
 package day06;
 
+import java.util.Scanner;
+
 public class Q05_ForLoop {
+    /*
+   Get the number of rows and columns from user.
+   Create a rectangle.
+   rows = 3 and column = 5 ==> * * * * *
+                               * * * * *
+                               * * * * *
+*/
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("diktörtgen için satır sayısı girin");
+        int satır= scan.nextInt();
+        System.out.println("diktörtgen için sütun sayısı girin");
+        int sütun= scan.nextInt();
+        for (int i = 1; i <= satır; i++) {
+            for (int j = 0; j <=sütun ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+
+    }
+
+
 }
---
+++ src/day06/Q06_ForLoop.java
@@ -0,0 +1,4 @@
+package day06;
+
+public class Q06_ForLoop {
+}
